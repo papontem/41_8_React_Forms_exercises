5c86757f3f30b6d9616e5716c7fe017ce0930c7d
papontemjetix <papontem@gmail.com>
2023-12-07 21:27:02 -0500

    added logic from code demo to the newBoxForm component, tests still passing.

--- part_1_Color_Box_Maker/color_box_maker/src/NewBoxForm.js
+++ part_1_Color_Box_Maker/color_box_maker/src/NewBoxForm.js
@@ -1,12 +1,71 @@
+import React, { useState } from "react";
 /* 
     Renders a form that allows users to specify the width, height, and background color for a new box.
     When the form is submitted, it creates a new box with the specified properties and clears the input values.
 */
 function NewBoxForm(params) {
+	// State for form data
+	// width, height, and background color for a new box.
+	const [newBoxFormData, setNewBoxFormData] = useState({
+		width: "",
+		height: "",
+		backgroundColor: "",
+	});
+
+	// Event handler for form input changes
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setNewBoxFormData((prevData) => ({
+			...prevData,
+			[name]: value,
+		}));
+	};
+
+	// Event handler for form submission
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// PAM: Well add more logic here, like clearing values on the form
+		console.log("Form submitted:", newBoxFormData);
+	};
+
 	return (
 		<div className="NewBoxForm">
+			<h2>New Box Form</h2>
 			{/*  */}
-			<p>lorem ipsum</p>
+			<form onSubmit={handleSubmit}>
+				<label>
+					width:
+					<input
+						type="text"
+						name="width"
+						value={newBoxFormData.width}
+						onChange={handleChange}
+						required></input>
+				</label>
+				<br />
+				<label>
+					height:
+					<input
+						type="text"
+						name="height"
+						value={newBoxFormData.height}
+						onChange={handleChange}
+						required></input>
+				</label>
+				<br />
+				<label>
+					backgroundColor:
+					<input
+						type="text"
+						name="backgroundColor"
+						value={newBoxFormData.backgroundColor}
+						onChange={handleChange}
+						required></input>
+				</label>
+				<br />
+
+				<button type="submit">Add New Box!</button>
+			</form>
 		</div>
 	);
 }
